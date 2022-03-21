0c98ff260d6dd0c97fb79bac71df7d69427832c6
AlexCLarkin <98946206+AlexCLarkin@users.noreply.github.com>
2022-03-21 16:26:43 +0000

    Add files via upload
    probably the final version

--- Crud app/javascript/crud.js
+++ Crud app/javascript/crud.js
@@ -33,8 +33,6 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Click on the edit button to open prompt and edit task
-document.getElementsByClassName("editBtn").addEventListener("click", editTask);
-
 function editTask() {
     var editedTask = prompt("Write updated task here...");
     if (editedTask === "") {
@@ -114,41 +112,6 @@ function newElement() {
     };
   }
   var btn = document.createElement("button");
-  var txt = document.createTextNode("✎");
-  btn.className = "editBtn";
-  btn.addEventListener("click", editTask);
-  btn.appendChild(txt);
-  li.appendChild(btn);
-}
-
-//add new element with the class priority
-function newElementPriority() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.className = "priority"
-    li.appendChild(t);
-    if (inputValue === "") {
-      alert("Task can't be blank!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-      li.id = "li-text";
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-      };
-    }
-    var btn = document.createElement("button");
   var txt = document.createTextNode("✎");
   btn.className = "editBtn";
   btn.addEventListener("click", editTask);
